28baab6aa255de6e6daf70bba8bc7e823b8a8760
Nagendra <nagendra.burusu@thoughtclan.com>
2026-06-08 16:18:33 +0530

    basic config

---
+++ src/main/java/com/example/azure_servicebus_reference_app/AzureServicebusReferenceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.azure_servicebus_reference_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureServicebusReferenceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureServicebusReferenceAppApplication.class, args);
+	}
+
+}
